a387eff6d6a8371759451a9d9b4c2f62c74a98bf
itujun <itujun7@gmail.com>
2023-11-23 15:37:23 +0700

    interaksi pertama: findMany()

--- utils/getQueryOutput.js
+++ utils/getQueryOutput.js
@@ -1,5 +1,18 @@
 // File biasa untuk berinteraksi dengan database
-export const queryData = async() => {
-    const queryOutput = 'Hello World!';
+// Task 1
+// export const queryData = async() => {
+//     const queryOutput = 'Hello World!';
+//     return queryOutput;
+// };
+
+import prisma from './PrismaClient';
+
+// Task 2 : findMany()
+export const queryData = async () => {
+  try {
+    const queryOutput = await prisma.users.findMany();
     return queryOutput;
+  } catch (error) {
+    console.log(error);
+  }
 };
