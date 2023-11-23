f0d9ebd9740ef3b76f6e05baf07d4efd6c7c6c9a
itujun <itujun7@gmail.com>
2023-11-23 16:27:26 +0700

    Task 16: aggregate()

--- utils/getQueryOutput.js
+++ utils/getQueryOutput.js
@@ -1,11 +1,17 @@
 import prisma from './PrismaClient';
 
-// Task 15 : count()
+// Task 16 : aggregate()
 export const queryData = async () => {
   try {
-    const queryOutput = await prisma.users.count({
-      where: {
-        validated: false,
+    const queryOutput = await prisma.users.aggregate({
+      _min: {
+        createdAt: true,
+      },
+      _max: {
+        createdAt: true,
+      },
+      _count: {
+        createdAt: true,
       },
     });
     return queryOutput;
