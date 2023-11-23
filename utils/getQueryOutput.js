16cf81ba5b97cac27ad0cef4393d92714565d101
itujun <itujun7@gmail.com>
2023-11-23 16:42:31 +0700

    Task 19:

--- utils/getQueryOutput.js
+++ utils/getQueryOutput.js
@@ -1,12 +1,10 @@
 import prisma from './PrismaClient';
 
-// Task 18 : skip + take + findMany()
+// Task 19 : $queryRaw
+// Sebaiknya jangan digunakan, karena rentan terhadap serangan SQL misalnya seperti Sql Injection
 export const queryData = async () => {
   try {
-    const queryOutput = await prisma.users.findMany({
-      skip: 0,
-      take: 2,
-    });
+    const queryOutput = await prisma.$queryRaw`SELECT * FROM cobanextprisma.users`;
     return queryOutput;
   } catch (error) {
     console.log(error);
