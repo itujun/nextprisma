a9e06e2a842cdfdcd74cf7c0c0cc339cc3fff2d8
itujun <itujun7@gmail.com>
2023-11-23 15:47:13 +0700

    Task 4: findMany() + select + where

--- utils/getQueryOutput.js
+++ utils/getQueryOutput.js
@@ -1,31 +1,15 @@
-// File biasa untuk berinteraksi dengan database
-// Task 1
-// export const queryData = async() => {
-//     const queryOutput = 'Hello World!';
-//     return queryOutput;
-// };
-
 import prisma from './PrismaClient';
 
-// Task 2 : findMany()
-// export const queryData = async () => {
-//   try {
-//     const queryOutput = await prisma.users.findMany();
-//     return queryOutput;
-//   } catch (error) {
-//     console.log(error);
-//   } finally {
-//     prisma.$disconnect();
-//   }
-// };
-
-// Task 3 : findMany() + Select
+// Task 4 : findMany() + Select + Where
 export const queryData = async () => {
   try {
     const queryOutput = await prisma.users.findMany({
       select: {
         firstName: true,
         lastName: true,
+      },
+      where: {
+        firstName: 'Juna',
       },
     });
     return queryOutput;
